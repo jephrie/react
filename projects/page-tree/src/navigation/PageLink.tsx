62ec6628b97365d4dc8088f5867f444df65d726b
jephrie <championman@hotmail.com>
2025-08-25 12:15:29 +1000

    click the nav link to read the associated page

--- projects/page-tree/src/navigation/PageLink.tsx
+++ projects/page-tree/src/navigation/PageLink.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { BaseSyntheticEvent, useContext, useState } from 'react';
 import { AddPageButton } from '../page/AddPageButton';
+import { StoreContext } from '../store/Store';
 
 type Props = {
     id: string;
@@ -8,12 +9,17 @@ type Props = {
 
 export const PageLink = ({ id, title }: Props) => {
     const [isMouseOver, setIsMouseOver] = useState(false);
+    const { currentPageId, updateCurrentPageId, updateMode } = useContext(StoreContext);
+    const onClick = (e: BaseSyntheticEvent) => {
+        updateCurrentPageId(id);
+        updateMode('ReadPage');
+    };
 
     return (
         <li>
             <div className='nav-link' onMouseEnter={() => setIsMouseOver(true)} onMouseLeave={() => setIsMouseOver(false)}>
                 <div className='nav-link-title'>
-                    {title}
+                    {currentPageId === id ? title : <a href='#' onClick={onClick}>{title}</a>}
                 </div>
                 {isMouseOver && <AddPageButton label='+' appearance='small' parentPageId={id} />}
             </div>
