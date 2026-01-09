bcbd694db2fa4698dc73a03b47c9bf214614f749
jephrie <championman@hotmail.com>
2026-01-09 14:41:51 +1100

    users can search to filter page titles in nav

--- projects/page-tree/src/navigation/Navigator.tsx
+++ projects/page-tree/src/navigation/Navigator.tsx
@@ -1,13 +1,20 @@
-import { useContext } from 'react';
+import { ChangeEvent, useCallback, useContext, useState } from 'react';
 import './Navigator.css';
 import { PageLink } from './PageLink';
 import { AddPageButton } from '../page/AddPageButton';
 import { StoreContext } from '../store/Store';
+import { Page } from '../common/Types';
 
 export const Navigator = () => {
     const { pages } = useContext(StoreContext);
-    const allPageIds = Object.keys(pages);
+    const [ searchTerm, setSearchTerm ] = useState<string>();
 
+    const onSearchChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        setSearchTerm(e.target.value);
+    }, []);
+
+    const allPageIds = Object.keys(pages);
     if (allPageIds.length === 0) {
         return (
             <div className='navigator-pane'>
@@ -16,9 +23,33 @@ export const Navigator = () => {
         )
     }
 
-    const topLevelPages = allPageIds.filter((pageId: string) => !pages[pageId].parent);
+    const getFilteredPages = () => {
+        // Set to remove duplicates if a page is matched via title and via ancestry from a child match.
+        const matchingPages = new Set<string>();
+        const matchingLeafPages = allPageIds.filter((pageId: string) => pages[pageId].title.includes(searchTerm!));
+
+        const traverseAndAddAncestors = (page: Page) => {
+            if (page.parent) {
+                traverseAndAddAncestors(pages[page.parent]);
+                matchingPages.add(page.parent);
+            }
+        };
+        matchingLeafPages.forEach((pageId: string) => {
+            traverseAndAddAncestors(pages[pageId]);
+            matchingPages.add(pageId);
+        });
+
+        return matchingPages;
+    };
+    // Set instead of array to improve performance. This set could be checked frequently when generating tree.
+    const filteredPages = searchTerm ? getFilteredPages() : new Set(allPageIds);
+    const topLevelPages = Array.from(filteredPages).filter((pageId: string) => !pages[pageId].parent);
     const generateTree = (pageId: string) => {
         const page = pages[pageId];
+
+        if (!filteredPages.has(pageId)) {
+            return;
+        }
 
         return (
             <ul className='nav-tree' key={`page-title-${pageId}`}>
@@ -31,6 +62,7 @@ export const Navigator = () => {
     return (
         <div className='navigator-pane'>
             <div>
+                <div><input className='search-bar' placeholder='Search' onChange={onSearchChange}/></div>
                 {topLevelPages.map((pageId: string) => generateTree(pageId))}
             </div>
             <AddPageButton label='Create new page' appearance='medium' />
